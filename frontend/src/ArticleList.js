1e4f344e70f0a8865edc52f42a5c6e4dca88837e
Elliot <hack00mind@gmail.com>
2021-04-11 17:51:09 +0800

    feat(frontend): beautify the page

--- frontend/src/ArticleList.js
+++ frontend/src/ArticleList.js
@@ -1,36 +1,44 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn"
+import relativeTime from "dayjs/plugin/relativeTime"
+
+dayjs.locale('zh-cn')
+dayjs.extend(relativeTime)
 
 class ArticleList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        articleList: [],
+      articleList: [],
     }
   }
 
   componentDidMount() {
     fetch('/api/articles/')
       .then(response => response.json())
-      .then(result => this.setState({articleList: result}))
+      .then(result => this.setState({ articleList: result }))
       .catch(e => console.error(e));
   }
 
   render() {
-      console.log(this.state.articleList)
-      return (
-      <div>
-        {this.state.articleList.map(item =>
-          <div key={item.id}>
-            <h4>{item.title}</h4>
-            <p>
-              <strong>{item.body}</strong>
-              <br/>
-              <em>创建时间：{item.created}</em>
-              <em>更新时间：{item.updated}</em>
-            </p>
-          </div>
-        )}
-      </div>
+    const { articleList } = this.state;
+    return (
+      Array.isArray(articleList) && articleList.length !== 0
+        ?
+        <div className="font-sans">
+          {articleList.map(item =>
+            <div key={item.id} className="py-3">
+              <div className="text-2xl font-semibold">{item.title}</div>
+              <div className="space-x-2">
+                <span>创建时间：<time title={item.created}>{dayjs().to(dayjs(item.created))}</time></span>
+                <span>更新时间：<time title={item.updated}>{dayjs().to(dayjs(item.updated))}</time></span>
+              </div>
+            </div>
+          )}
+        </div>
+        :
+        <div>Loading</div>
     );
   }
 
